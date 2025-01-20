d5361104c8705993f233f0a3c0333743d2474a3a
GabSilv219 <gabsilv219@gmail.com>
2025-01-20 19:16:03 -0400

    audio test 2

--- js/audio.js
+++ js/audio.js
@@ -1,3 +1,17 @@
+// const audio = document.getElementById("audio");
+// audio.volume = 0.1;
+// audio.play();
+
 const audio = document.getElementById("audio");
+const playButton = document.getElementById("playButton");
 audio.volume = 0.1;
-audio.play();
+
+playButton.addEventListener('click', function() {
+    if (audio.paused) {
+        audio.play();
+        playButton.textContent = "⏸️ Pausar";
+    } else {
+        audio.pause();
+        playButton.textContent = "▶️ Play Música";
+    }
+});
